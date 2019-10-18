18ad69dd77e44c4e7bd273564127f47c8840eb66
David Rhodes <rhodesdav@gmail.com>
2019-10-18 12:33:34 -0500

    test(stackform): adds dynamic tests for actions

--- src/components/StackForm.test.js
+++ src/components/StackForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, simulate } from 'enzyme';
 import { StackForm } from './StackForm';
 import { stack } from '../data/fixtures';
 
@@ -27,6 +27,41 @@ describe('StackForm component', () => {
     it('renders a button to submit a new stack', () => {
         expect(stackForm.find('Button').at(1).props().children).toEqual('Save Stack')
     });
+
+    describe('and updating the title', () => {
+        const CHANGED_TITLE = 'Changed Title';
+
+        beforeEach(()=> {
+            stackForm.find('FormControl').simulate('change', {target: {
+                value: CHANGED_TITLE
+            }})
+        })
+
+        it('updates the title in state', () => {
+            expect(stackForm.state().title).toEqual(CHANGED_TITLE )
+        })
+    })
+
+    describe('when adding a new card', () => {
+        const PROMPT = 'Why did the chicken cross the road?';
+        const ANSWER = 'To get to the other side.';
+
+        beforeEach(()=> {
+            stackForm.find('Button').at(0).simulate('click')
+        })
+
+        it('adds a new card to the state', () => {
+            expect(stackForm.state().cards.length).toEqual(1)
+        })
+
+        it('renders the prompt call to action', () => {
+            expect(stackForm.find('FormLabel').at(1).props().children).toEqual('Prompt:');
+        })
+
+        it('renders the prompt call to action', () => {
+            expect(stackForm.find('FormLabel').at(2).props().children).toEqual('Answer:');
+        })
+    })
 
 
 });
