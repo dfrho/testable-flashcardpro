301f50f5d63350b4396e5eb29f9364a6fa88cabb
David Rhodes <rhodesdav@gmail.com>
2019-10-19 08:39:05 -0500

    test(reducers): reducers covered

---
+++ src/reducers/index.test.js
@@ -0,0 +1,34 @@
+import rootReducer from './index';
+import * as actions from '../actions/';
+import { stack, stacks } from '../data/fixtures';
+
+
+describe ('root reducer', () => {
+    it('returns the initial state', () => {
+        expect(rootReducer({}, {})).toEqual({
+            stack: {},
+            stacks: []
+        });
+    })
+
+    it('sets the current stack', () => {
+        expect(rootReducer({}, {type: actions.SET_STACK, stack})).toEqual({
+            stack,
+            stacks: []
+        });
+    })
+
+    it('loads stacks', () => {
+        expect(rootReducer({}, {type: actions.LOAD_STACKS, stacks})).toEqual({
+            stack: {},
+            stacks
+        });
+    })
+
+    it('adds a stack', () => {
+        expect(rootReducer({}, {type: actions.ADD_STACK, stack})).toEqual({
+            stack: {},
+            stacks: [{...stack, id: 0}]
+        });
+    })
+})
